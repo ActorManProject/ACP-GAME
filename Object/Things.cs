e1854e438fe308b06de40bfd3e1c013095bb0737
JeHwanYoo <formail0001@naver.com>
2016-02-09 17:29:14 +0900

    abstract 클래스임. 모든 오브젝트 스크립트의 모체가 됨

---
+++ Object/Things.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract public class Things : MonoBehaviour {
+
+	protected int id;
+	public float health;
+	public float shield;
+
+	protected virtual void Start()
+	{
+		
+		// To Connect DataCenter with This Obejct
+		GameObject DataCenter = GameObject.Find("DataCenter");
+		DataCenter.GetComponent<DataCenter>().Connect(this);
+	
+	}
+
+	public void Set_Id(int _id) { id = _id; }
+
+	public int Get_Id() { return id; }
+}
