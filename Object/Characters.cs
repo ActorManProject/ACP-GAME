0a704a2a012ed0ff54b2654e222337f6ddd25ed3
JeHwanYoo <formail0001@naver.com>
2016-02-09 17:27:50 +0900

    player등에 사용하면 됨, Things를 상속 받음

---
+++ Object/Characters.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Characters : Things {
+
+	public float move_speed;
+
+	Characters()
+	{
+		health = 100;
+		shield = 20;
+		move_speed = 1;
+	}
+
+	// Use this for initialization
+	protected override void Start () {
+		
+		// To be connected with DataCenter
+		base.Start();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
