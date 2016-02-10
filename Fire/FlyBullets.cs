bc703b81b9c2a047b858fe2a5743e04546075d8a
JeHwanYoo <formail0001@naver.com>
2016-02-10 18:43:05 +0900

    총알 날아감

---
+++ Fire/FlyBullets.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyBullets : MonoBehaviour {
+	
+	public float bulletSpeed;
+
+	FlyBullets()
+	{
+		bulletSpeed = 30f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Destroy(gameObject);
+	}
+	
+}
