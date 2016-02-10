ee2357bc9596625c03b2b5a8410a0c2126408a24
JeHwanYoo <formail0001@naver.com>
2016-02-10 15:26:41 +0900

    데이터센터 통신 업데이트

--- Object/Things.cs
+++ Object/Things.cs
@@ -11,9 +11,19 @@ abstract public class Things : MonoBehaviour {
 	{
 		
 		// To Connect DataCenter with This Obejct
-		GameObject DataCenter = GameObject.Find("DataCenter");
+		var DataCenter = GameObject.Find("DataCenter");
 		DataCenter.GetComponent<DataCenter>().Connect(this);
 	
+	}
+
+	void OnDestroy() 
+	{	
+		// when destroy, remove missing pointer
+		if(health <= 0) // 정삭적인 죽음에 대해서만
+		{
+			var DataCenter = GameObject.Find("DataCenter");
+			DataCenter.GetComponent<DataCenter>().Delete_List(id);
+		}			
 	}
 
 	public void Set_Id(int _id) { id = _id; }
